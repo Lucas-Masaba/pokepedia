5833c3723443bab27ff7215c674741c5a26a6ca8
Lucas-Masaba <masabaluke@gmail.com>
2022-06-27 18:54:36 +0300

    header padding

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Header = () => (
-  <header className="flex bg-gray-200 justify-between p-3 flex-row align-baseline">
+  <header className="flex sticky top-0 z-50 bg-gray-200 justify-between p-2 flex-row align-baseline">
     <img className="animate-bounce" src="/pokeball.png" alt="pokeball" width="60px"/>
     <span className="pt-3 font-Poppins">Pokepedia</span>
   </header>
