62d7e330d68349c022fc7b55ce3826983f35bcba
Lucas-Masaba <masabaluke@gmail.com>
2022-07-26 19:24:42 +0300

    add color change function in different file of different folder

---
+++ public/helper_functions/color_changer.js
@@ -0,0 +1,28 @@
+const colorChange = (type) => {
+  switch (type) {
+    case "fire":
+      const red = {
+        background: "#b91c1c",
+      };
+      return red;
+    case "water":
+      const blue = {
+        background: "#2563eb",
+      };
+      return blue;
+    case "normal":
+      const yellow = {
+        color: '#000000',
+        background: "#facc15",
+      };
+      return yellow;
+    default:
+      const defaultColour = {
+        color: '#000000',
+        background: "facc15",
+      };
+      return defaultColour;
+  }
+};
+
+export default colorChange
