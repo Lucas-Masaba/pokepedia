fd94edcfb1180b95b640e882af1e0c6ef235c733
Lucas-Masaba <masabaluke@gmail.com>
2022-07-27 23:48:36 +0300

    Modify design

--- src/components/pokeImage.jsx
+++ src/components/pokeImage.jsx
@@ -4,7 +4,7 @@ import { useGetPokemonByNameQuery } from "../redux/pokemon/pokemon";
 const Images = ({ pokeName }) => {
   const { data, isLoading, error } = useGetPokemonByNameQuery(pokeName);
   return (
-    <div className="flex items-center justify-center shadow-inner shadow-cyan-500 rounded-full h-[150px] w-[150px] bg-white">
+    <div className="flex items-center justify-center shadow-inner  rounded-full h-[150px] w-[150px] bg-white">
       {error && <>Oops, something went wrong</>}
       {isLoading && (
         <img className="animate-pulse" src="/pokeball.png" alt="" />
