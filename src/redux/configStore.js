c5b7cb1320ea52bdae460aa924082a007aa17536
Lucas-Masaba <masabaluke@gmail.com>
2022-10-11 20:34:55 +0300

    modify redux store

--- src/redux/configStore.js
+++ src/redux/configStore.js
@@ -4,11 +4,13 @@ import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 
 import { pokemonApi } from './pokemon/pokemon';
+import { login } from './login/login'
 
 export const store = configureStore({
   reducer: {
     // Add the generated reducer as a specific top-level slice
     [pokemonApi.reducerPath]: pokemonApi.reducer,
+    [login.reducerPath]: login.reducer,
   },
   // Adding the api middleware enables caching, invalidation, polling,
   // and other useful features of `rtk-query`.
